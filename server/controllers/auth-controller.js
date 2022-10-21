4a36e3dcbb3c67921c19b3aae36223a0b201852e
Sung Mo Yang <sungmo.yang@stonybrook.edu>
2022-10-20 22:27:46 -0400

    revised filename

---
+++ server/controllers/auth-controller.js
@@ -0,0 +1,305 @@
+const auth = require('../auth')
+const User = require('../models/user-model')
+const bcrypt = require('bcryptjs');
+
+loggedIn = async (req, res) => {
+    try {
+        let userId = auth.verifyUser(req);
+        if (!userId) {
+            return res.status(200).json({
+                loggedIn: false,
+                user: null,
+                errorMessage: "?"
+            })
+        }
+
+        const loggedInUser = await User.findOne({ _id: userId });
+        console.log("loggedInUser: " + loggedInUser);
+
+        return res.status(200).json({
+            loggedIn: true,
+            user: {
+                firstName: loggedInUser.firstName,
+                lastName: loggedInUser.lastName,
+                email: loggedInUser.email,
+                joinDate: loggedInUser.joinDate
+            }
+        })
+    } catch (err) {
+        console.log("err: " + err);
+        res.json(false);
+    }
+}
+
+login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res
+                .status(400)
+                .json({ errorMessage: "Please enter all required fields." });
+        }
+
+        const existingUser = await User.findOne({ email: email });
+        console.log("existingUser: " + existingUser);
+        if (!existingUser) {
+            return res
+                .status(401)
+                .json({
+                    errorMessage: "Wrong username or password provided."
+                })
+        }
+
+        console.log("provided password: " + password);
+        const passwordCorrect = await bcrypt.compare(password, existingUser.passwordHash);
+        if (!passwordCorrect) {
+            console.log("Incorrect password");
+            return res
+                .status(401)
+                .json({
+                    errorMessage: "Wrong email or password provided."
+                })
+        }
+
+        // LOGIN THE USER
+        const token = auth.signToken(existingUser._id);
+        console.log(token);
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: true
+        }).status(200).json({
+            success: true,
+            user: {
+                firstName: existingUser.firstName,
+                lastName: existingUser.lastName,  
+                email: existingUser.email              
+            }
+        })
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+}
+
+logout = async (req, res) => {
+    try {
+        res.cookie("token", "", {
+            httpOnly: true,
+            expires: new Date(0),
+            secure: true,
+            sameSite: true
+        }).send();
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+}
+
+register = async (req, res) => {
+    try {
+        const { firstName, lastName, email, password, passwordVerify } = req.body;
+        console.log("create user: " + firstName + " " + lastName + " " + email + " " + password + " " + passwordVerify);
+        if (!firstName || !lastName || !email || !password || !passwordVerify) {
+            return res
+                .status(400)
+                .json({ errorMessage: "Please enter all required fields." });
+        }
+        console.log("all fields provided");
+        if (password.length < 8) {
+            return res
+                .status(400)
+                .json({
+                    errorMessage: "Please enter a password of at least 8 characters."
+                });
+        }
+        console.log("password long enough");
+        if (password !== passwordVerify) {
+            return res
+                .status(400)
+                .json({
+                    errorMessage: "Please enter the same password twice."
+                })
+        }
+        console.log("password and password verify match");
+        const existingUser = await User.findOne({ email: email });
+        console.log("existingUser: " + existingUser);
+        if (existingUser) {
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    errorMessage: "An account with this email address already exists."
+                })
+        }
+
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const passwordHash = await bcrypt.hash(password, salt);
+        console.log("passwordHash: " + passwordHash);
+
+        const newUser = new User({
+            firstName, lastName, email, passwordHash
+        });
+        const savedUser = await newUser.save();
+        console.log("new user saved: " + savedUser._id);
+
+        // LOGIN THE USER
+        const token = auth.signToken(savedUser._id);
+        console.log("token:" + token);
+
+        await res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        }).status(200).json({
+            success: true,
+            user: {
+                firstName: savedUser.firstName,
+                lastName: savedUser.lastName,  
+                email: savedUser.email              
+            }
+        })
+
+        console.log("token sent");
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+}
+
+changePassword = async (req, res) => {
+    try {
+        const { password, passwordVerify } = req.body;
+        console.log("change password: " + password + " " + passwordVerify);
+        if (!password || !passwordVerify) {
+            return res
+                .status(400)
+                .json({ errorMessage: "Please enter all required fields." });
+        }
+        console.log("all fields provided");
+        if (password.length < 8) {
+            return res
+                .status(400)
+                .json({
+                    errorMessage: "Please enter a password of at least 8 characters."
+                });
+        }
+        console.log("password long enough");
+        if (password !== passwordVerify) {
+            return res
+                .status(400)
+                .json({
+                    errorMessage: "Please enter the same password twice."
+                })
+        }
+        console.log("password and password verify match");
+
+        const userId = auth.verifyUser(req);
+        if (!userId) {
+            return res.status(200).json({
+                success: false,
+                errorMessage: "You must be logged in to change your password."
+            })
+        }
+
+        const existingUser = await User.findOne({ _id: userId });
+        console.log("existingUser: " + existingUser);
+        if (!existingUser) {
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    errorMessage: "An account with this email address does not exist."
+                })
+        }
+
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const passwordHash = await bcrypt.hash(password, salt);
+        console.log("passwordHash: " + passwordHash);
+
+        const updatedUser = await User.findOneAndUpdate({ _id: userId }, { passwordHash: passwordHash });
+        console.log("updatedUser: " + updatedUser);
+
+        // LOGIN THE USER
+        const token = auth.signToken(updatedUser._id);
+        console.log("token:" + token);
+
+        await res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        }).status(200).json({
+            success: true,
+            user: {
+                firstName: updatedUser.firstName,
+                lastName: updatedUser.lastName,
+                email: updatedUser.email
+            }
+        })
+
+        console.log("token sent");
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+}
+
+deleteAccount = async (req, res) => {
+    try {
+        const userId = auth.verifyUser(req);
+        if (!userId) {
+            return res.status(200).json({
+                success: false,
+                errorMessage: "You must be logged in to delete your account."
+            })
+        }
+
+        const existingUser = await User.findOne({ _id: userId });
+        console.log("existingUser: " + existingUser);
+        if (!existingUser) {
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    errorMessage: "An account with this email address does not exist."
+                })
+        }
+
+        const deletedUser = await User.findOneAndDelete({ _id: userId });
+        console.log("deletedUser: " + deletedUser);
+
+        res.cookie("token", "", {
+            httpOnly: true,
+            expires: new Date(0),
+            secure: true,
+            sameSite: true
+        }).status(200).json({
+            success: true,
+            user: {
+                firstName: deletedUser.firstName,
+                lastName: deletedUser.lastName,
+                email: deletedUser.email
+            }
+        })
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+}
+
+module.exports = {
+    loggedIn,
+    login,
+    logout,
+    register,
+    changePassword,
+    deleteAccount
+}
