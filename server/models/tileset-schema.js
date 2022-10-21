50c90fd058fc8b5e479a342be4c1525b9a9b18a1
AlamTahmidul <mohammed.alam.217@gmail.com>
2022-10-20 22:20:28 -0400

    TilesetSchema created
    - Don't Need `package-lock.json`
    - Added TilsetSchema

---
+++ server/models/tileset-schema.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectID = Schema.Types.ObjectId;
+
+const TilesetSchema = new Schema(
+    {
+        owner: { type: ObjectID, ref: 'User', required: true },
+        name: { type: String, required: true },
+        tags: { type: ObjectID, ref: 'Tag' },
+        views: { type: Number },
+        likedUsers: [{type: ObjectID, ref: 'User'}],
+        dislikedUsers: [{type: ObjectID, ref: 'User'}],
+        comments: [ {type: ObjectID, ref: 'Comment'} ],
+        deleted: { type: Boolean },
+        collaborators: [ {type: ObjectID, ref: 'User'} ],
+        viewers: [ {type: ObjectID, ref: 'User'} ],
+        public: { type: Boolean },
+        image: { type: String },
+        imageHeight: { type: Number },
+        imageWidth: { type: Number },
+        tileSize: { type: Number },
+        tileCount: { type: Number }
+    },
+    { timestamps: true },
+);
+
+module.exports = mongoose.model('Tileset', TilesetSchema);
