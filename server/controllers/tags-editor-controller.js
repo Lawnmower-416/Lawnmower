81042e13f3f4e793d916b2e7551f8ab15f01ede6
Rohit Panda <rohitpanda2@yahoo.com>
2022-10-23 14:37:38 -0400

    tags editor controller commit 2

---
+++ server/controllers/tags-editor-controller.js
@@ -0,0 +1,91 @@
+const databaseManager = require("AWSManager/tagseditor-controller");
+
+function createTag(req, res) {
+    const {name} = req.body;
+    if (!name) {
+        return res.status(400).json({
+            errorMessage: 'Improperly formatted request'
+        });
+    }
+    let newTag = databaseManager.createTag(name, req.user._id);
+    if (newTag) {
+        return res.status(200).json({
+            successMessage: 'Tag created',
+            tag: newtag
+        });
+    } else {
+        return res.status(400).json({
+            errorMessage: 'Unable to create tag'
+        });
+    }
+}
+
+function getTag(req, res) {
+    const name = req.body;
+    if (!name) {
+        return res.status(400).json({
+            errorMessage: 'Improperly formatted request'
+        });
+    } else {
+        let tag = databaseManager.getTag(body, req.tagId);
+        if (tag) {
+            return res.status(200).json({
+                successMessage: 'Tag found',
+                tag: tag
+            });
+        } else {
+            return res.status(400).json({
+                errorMessage: 'Unable to find tag'
+            });
+        }
+    }
+}
+
+function updatetag(req, res) {
+    const name = req.body;
+    if (!name) {
+        return res.status(400).json({
+            errorMessage: 'Improperly formatted request'
+        });
+    } else {
+        let tag = databaseManager.updateTag(body, req.user._id);
+        if (tag) {
+            return res.status(200).json({
+                successMessage: 'tag updated',
+                tag: tag
+            });
+        } else {
+            return res.status(400).json({
+                errorMessage: 'Unable to update tag'
+            });
+        }
+    }
+}
+
+function deletetag(req, res) {
+    const name = req.body;
+    if (!name) {
+        return res.status(400).json({
+            errorMessage: 'Improperly formatted request'
+        });
+    } else {
+        let tag = databaseManager.deletetag(body, req.user._id);
+        if (tag) {
+            return res.status(200).json({
+                successMessage: 'tag deleted',
+                tag: tag
+            });
+        } else {
+            return res.status(400).json({
+                errorMessage: 'Unable to delete tag'
+            });
+        }
+    }
+}
+
+module.exports = {
+    createTag,
+    getTag,
+    updateTag,
+    deleteTag
+}
