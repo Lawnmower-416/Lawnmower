c86a0cb6ed219af7c4b6a4e52a1c2dda684a18cc
AlamTahmidul <mohammed.alam.217@gmail.com>
2022-10-23 15:33:10 -0400

    UPDATE-tileseteditor-controller-returns
    Some bug fixes too.

--- server/controllers/tileset-editor-controller.js
+++ server/controllers/tileset-editor-controller.js
@@ -1,50 +1,45 @@
-// router.post('/map/:mapId/layer/:layerId/property', auth.verify, PropertyEditorController.createProperty)
-// router.get('/map/:mapId/layer/:layerId/property/:propertyId', auth.verify, PropertyEditorController.getProperty)
-// router.put('/map/:mapId/layer/:layerId/property/:propertyId', auth.verify, PropertyEditorController.updateProperty)
-// router.delete('/map/:mapId/layer/:layerId/property/:propertyId', auth.verify, PropertyEditorController.deleteProperty)
-
 const databaseManager = require('./AWSManager/tileseteditor-controller')
 
 function createTileset(req, res) {
-    const body = req.body
+    const body = req.body;
     if (!body) {
         return res.status(400).json({
             success: false,
-            error: 'Improperly formatted request'',
-        })
+            error: 'Improperly formatted request',
+        });
     }
-    let newTileset = databaseManager.createTileset(body, req.user._id)
+    let newTileset = databaseManager.createTileset(body);
     if (newTileset) {
         return res.status(200).json({
             success: true,
             tileset: newTileset,
-        })
-    }
+        });
+    };
     return res.status(400).json({
         success: false,
         error: 'Unable to create tileset'
-    })
+    });
 }
 
 function getTilesetsForMapById(req, res) {
-    const body = req.body
+    const body = req.body;
     if (!body) {
         return res.status(400).json({
             success: false,
             error: 'Improperly formatted request'
         })
     }
-    let tilesets = databaseManager.getTilesetsForMapById(req.params.mapId, req.user._id)
+    let tilesets = databaseManager.getTilesetsForMapById(req.params.mapId);
     if (tilesets) {
         return res.status(200).json({
             success: true,
             tilesets: tilesets,
-        })
+        });
     }
     return res.status(404).json({
         success: false,
         error: 'Tilesets not found'
-    })
+    });
 }
 
 function getAllTilesets(req, res) {
@@ -53,19 +48,19 @@ function getAllTilesets(req, res) {
         return res.status(400).json({
             success: false,
             error: 'Improperly formatted request'
-        })
+        });
     }
-    let tilesets = databaseManager.getAllTilesets(req.user._id)
+    let tilesets = databaseManager.getAllTilesets();
     if (tilesets) {
         return res.status(200).json({
             success: true,
             tilesets: tilesets,
-        })
-    }
+        });
+    };
     return res.status(404).json({
         success: false,
         error: 'Tilesets not found'
-    })
+    });
 }
 
 function getATileset(req, res) {
@@ -76,49 +71,49 @@ function getATileset(req, res) {
             error: 'Improperly formatted request'
         })
     }
-    let tileset = databaseManager.getATileset(req.params.tilesetId, req.user._id)
+    let tileset = databaseManager.getATileset(req.params.tilesetId);
     if (tileset) {
         return res.status(200).json({
             success: true,
             tileset: tileset,
-        })
-    }
+        });
+    };
     return res.status(404).json({
         success: false,
         error: 'Tileset not found'
-    })
+    });
 }
 
 function updateTileset(req, res) {
-    const body = req.body
+    const body = req.body;
     if (!body) {
         return res.status(400).json({
             success: false,
             error: 'Improperly formatted request'
-        })
-    }
-    let tileset = databaseManager.updateTileset(req.params.tilesetId, req.user._id)
+        });
+    };
+    let tileset = databaseManager.updateTileset(req.params.tilesetId, req.body);
     if (tileset) {
         return res.status(200).json({
             success: true,
             tileset: tileset,
-        })
-    }
+        });
+    };
     return res.status(400).json({
         success: false,
         error: 'Unable to update tileset'
-    })
-}
+    });
+};
 
 function deleteTileset(req, res) {
-    const body = req.body
+    const body = req.body;
     if (!body) {
         return res.status(400).json({
             success: false,
             error: 'Improperly formatted request'
-        })
-    }
-    let tileset = databaseManager.deleteTileset(req.params.tilesetId, req.user._id)
+        });
+    };
+    let tileset = databaseManager.deleteTileset(req.params.tilesetId);
     if (tileset) {
         return res.status(200).json({
             success: true,
@@ -128,7 +123,7 @@ function deleteTileset(req, res) {
     return res.status(400).json({
         success: false,
         error: 'Unable to delete tileset'
-    })
+    });
 }
 
 module.exports = {
