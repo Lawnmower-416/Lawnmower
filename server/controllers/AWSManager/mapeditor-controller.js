b7e1fa09f3e64d001d9217fc97763356b1d0c270
Shreyas Prasad <shreyasprasad14@gmail.com>
2022-10-23 13:56:00 -0400

    Map Editor Controller

--- server/controllers/AWSManager/mapeditor-controller.js
+++ server/controllers/AWSManager/mapeditor-controller.js
@@ -1,20 +1,15 @@
 const Map = require("../../models/map-schema.js");
 
-module.exports.getMap = async (req, res) => {
-    const id = req.params.mapId;
-
-    await Map.findById(id, (err, map) => {
-        if(err) res.status(500).json({ success: false, errorMessage: err});
-        return res.status(200).json({ success: true, map: map});
+module.exports.getMap = async (mapId) => {
+    await Map.findById(mapId, (err, map) => {
+        if(err) return null;
+        return map;
     });
 }
 
-module.exports.updateMap = async (req, res) => {
-    const id = req.params.mapId;
-    const updatedMap = req.body;
-
-    await Map.findOneAndUpdate({ _id: id}, updatedMap, {new: true}, (err, map) => {
-        if(err) return res.status(500).json({ success: false, errorMessage: err});
-        return res.status(200).json({ success: true, tag: map});
+module.exports.updateMap = async (updatedMap) => {
+    await Map.findOneAndUpdate({ _id: updatedMap._id}, updatedMap, {new: true}, (err, map) => {
+        if(err) return null
+        return updatedMap;
     });
 }
---
+++ server/controllers/mapEditorController.js
@@ -0,0 +1,64 @@
+const databaseManager = require("AWSManager/mapeditor-controller");
+
+module.exports.getMap = async (req, res) => {
+    const id = req.params.mapId;
+    const userId = req.body.userId;
+
+    let map = databaseManager.getMap(id);
+
+    if(!map) {
+        return res.status(400).json({
+            success: false,
+            errorMessage: "Map not found"
+        });
+    }
+
+    if(!map.public) {
+        if(map.owner != userId && !map.collaborators.includes(userId)) {
+            return res.status(403).json({
+                success: false,
+                errorMessage: "You do not have permission to view this map."
+            });
+        }
+    }
+
+    return res.status(200).json({
+        success: true,
+        map: map
+    });
+}
+
+module.exports.updateMap = async (req, res) => {
+    const id = req.params.mapId;
+    const userId = req.body.userId;
+    const newMap = req.body.map;
+
+    let map = databaseManager.getMap(id);
+
+    if(!map) {
+        return res.status(400).json({
+            success: false,
+            errorMessage: "Map not found"
+        });
+    }
+
+    if(map.owner != userId && !map.collaborators.includes(userId)) {
+        return res.status(403).json({
+            success: false,
+            errorMessage: "You do not have permission to view this map."
+        });
+    }
+
+    let updatedMap = databaseManager.updateMap(newMap);
+    if(!updatedMap) {
+        return res.status(400).json({
+            success: false,
+            errorMessage: "Unable to update map"
+        });
+    } else {
+        return res.status(200).json({
+            success: true,
+            map: updatedMap
+        });
+    }
+}
