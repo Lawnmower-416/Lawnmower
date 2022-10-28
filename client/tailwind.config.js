c6720e7e091dc0265f5e98a0d3436c6371461960
SungMoY <90225118+SungMoY@users.noreply.github.com>
2022-10-28 18:14:01 -0400

    FEATURE-delete-account-modal

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'editor-primary': '#344E41',
+      'light-green': "#87C56C",
+      "dark-green": "#004500",
+      "red": "#D51300",
+      "white": "#FFFFFF",
+      "black": "#000000",
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
