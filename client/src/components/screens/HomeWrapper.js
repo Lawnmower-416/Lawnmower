4a5057fd0d58b8cedcfa5d4abed7d69eb87a9921
SungMoY <90225118+SungMoY@users.noreply.github.com>
2022-10-29 14:46:23 -0400

    added bottom content to page, fixed images, changed gradient

--- client/src/components/screens/HomeWrapper.js
+++ client/src/components/screens/HomeWrapper.js
@@ -4,15 +4,16 @@ import About from './About';
 import ContactUs from './ContactUs';
 import Faq from './Faq';
 import DeleteAccount from '../modals/DeleteAccount';
-import HeroSection from './HeroSection';
+import HomeScreen from './HomeScreen';
+import Header from './Header';
 
 
 export default function HomeWrapper() {
     console.log("in home wrapper");
     return (
         <div>
-            <DeleteAccount/>
-            <HeroSection/>
+            <Header/>
+            <HomeScreen/>
         </div>
     )
   }
