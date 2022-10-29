6b197a463a778846715d1578c0e6e3488fcd79e1
SungMoY <90225118+SungMoY@users.noreply.github.com>
2022-10-29 12:39:23 -0400

    UPDATE-routed-about-faq-contact

--- client/src/components/screens/HomeWrapper.js
+++ client/src/components/screens/HomeWrapper.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import DeleteContent from '../modals/DeleteContent';
-import About from './About';
-import ContactUs from './ContactUs';
-import Faq from './Faq';
-import DeleteAccount from '../modals/DeleteAccount';
+// import DeleteContent from '../modals/DeleteContent';
+// import About from './About';
+// import ContactUs from './ContactUs';
+// import Faq from './Faq';
+// //import DeleteAccount from '../modals/DeleteAccount';
 import HeroSection from './HeroSection';
+import Header from './Header';
 
 
 export default function HomeWrapper() {
     console.log("in home wrapper");
     return (
         <div>
-            <DeleteAccount/>
+            <Header />
             <HeroSection/>
         </div>
     )
