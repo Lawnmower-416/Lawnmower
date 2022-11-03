c5c84f0324ea4a4d9fc3d177d4852bec6ac614c1
SungMoY <90225118+SungMoY@users.noreply.github.com>
2022-11-02 23:01:09 -0400

    FEATURE-client-atuh
    Added authContext on client side.
    Added auth api on client side

---
+++ client/src/auth/auth-request-api/index.js
@@ -0,0 +1,68 @@
+/* Axios Library used to send HTTP requests to our back-end API
+    The baseURL should be modified to fit the ec2 backe-end instead of localhost */
+
+import axios from 'axios';
+axios.defaults.withCredentials = true;
+const api = axios.create({
+    baseURL: 'http://localhost:3000'
+});
+
+// THESE ARE ALL THE REQUESTS WE`LL BE MAKING, ALL REQUESTS HAVE A
+// REQUEST METHOD (like get) AND PATH (like /register). SOME ALSO
+// REQUIRE AN id SO THAT THE SERVER KNOWS ON WHICH LIST TO DO ITS
+// WORK, AND SOME REQUIRE DATA, WHICH WE WE WILL FORMAT HERE, FOR WHEN
+// WE NEED TO PUT THINGS INTO THE DATABASE OR IF WE HAVE SOME
+// CUSTOM FILTERS FOR QUERIES
+
+export const loggedIn = () => api.get(`/loggedIn/`);
+export const login = (username, password) => {
+    return api.post(`/login/`, {
+        username : username,
+        password : password
+    })
+}
+export const logout = () => api.get(`/logout/`)
+export const register = (firstName, lastName, username, email, password, passwordVerify) => {
+    return api.post(`/register/`, {
+        firstName : firstName,
+        lastName : lastName,
+        username : username,
+        email : email,
+        password : password,
+        passwordVerify : passwordVerify
+    })
+}
+export const changePassword = (password, passwordVerify) => {
+    return api.put(`/changePassword/`, {
+        password : password,
+        passwordVerify : passwordVerify
+    })
+}
+// This should be modified to check input username and password before deleting
+export const deleteAccount = () => api.delete(`/deleteAccount/`)
+
+/* Modified version. Front end and backend functions should be changed to fit this way
+export const deleteAccount = (username, password) => {
+    return api.delete(`/deleteAccount/`, {
+        username : username,
+        password : password
+    })
+}
+*/
+
+export const MapVerify = (mapId) => api.get(`/map/${mapId}/verify/`)
+
+export const TilesetVerify = (tilesetId) => api.get(`/tileset/${tilesetId}/verify/`)
+
+const apis = {
+    loggedIn,
+    login,
+    logout,
+    register,
+    changePassword,
+    deleteAccount,
+    MapVerify,
+    TilesetVerify
+}
+
+export default apis
---
+++ client/src/auth/index.js
@@ -0,0 +1,155 @@
+import React, { createContext, useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+// some functions use useHistory to redirect to a different page
+// check if this is necessary
+import api from '../api';
+
+const AuthContext = createContext();
+
+export const AuthActionType = {
+    GET_LOGGED_IN: 'GET_LOGGED_IN',
+    LOGIN: 'LOGIN',
+    LOGOUT: 'LOGOUT',
+    REGISTER: 'REGISTER',
+    CHANGE_PASSWORD_LOGGED_IN: 'CHANGE_PASSWORD_LOGGED_IN',
+    CHANGE_PASSWORD_NOT_LOGGED_IN: 'CHANGE_PASSWORD_NOT_LOGGED_IN',
+    DELETE_ACCOUNT: 'DELETE_ACCOUNT'
+}
+
+function AuthContextProvider(props) {
+    const [auth, setAuth] = useState({
+        user: null,
+        loggedIn: false
+    });
+
+    const history = useHistory();
+
+    useEffect(() => {
+        auth.loggedIn();
+    }, []);
+
+    const authReducer = (action) => {
+        const { type, payload } = action;
+        switch (type) {
+            case AuthActionType.GET_LOGGED_IN: {
+                return setAuth({
+                    user: payload.user,
+                    loggedIn: payload.loggedIn,
+                })
+            }
+            case AuthActionType.LOGIN: {
+                return setAuth({
+                    user: payload.user,
+                    loggedIn: true
+                })
+            }
+            case AuthActionType.LOGOUT: {
+                return setAuth({
+                    user: null,
+                    loggedIn: false
+                })
+            }
+            case AuthActionType.REGISTER: {
+                return setAuth({
+                    user: payload.user,
+                    loggedIn: true
+                })
+            }
+            case AuthActionType.CHANGE_PASSWORD: {
+                return setAuth({
+                    user: payload.user,
+                    loggedIn: true
+                })
+            }
+            case AuthActionType.DELETE_ACCOUNT: {
+                return setAuth({
+                    user: null,
+                    loggedIn: false
+                })
+            }
+            default:
+                return auth;
+        }
+    }
+
+    auth.loggedIn = async () => {
+        const response = await api.loggedIn();
+        if (response.status === 200) {
+            authReducer({
+                type: AuthActionType.GET_LOGGED_IN,
+                payload: {
+                    user: response.data.user,
+                    loggedIn: response.data.loggedIn
+                }
+            });
+        }
+    }
+    // setting to username, password. Requires changes in backend
+    // as backend is expecting email, password
+    auth.login = async (username, password) => {
+        const response = await api.login(username, password);
+        if (response.status === 200) {
+            authReducer({
+                type: AuthActionType.LOGIN,
+                payload: {
+                    user: response.data.user
+                }
+            });
+            history.push('/');
+        }
+    }
+    auth.logout = async () => {
+        const response = await api.logout();
+        if (response.status === 200) {
+            authReducer({
+                type: AuthActionType.LOGOUT,
+                payload: null
+            });
+            history.push('/');
+        }
+    }
+    auth.register = async (firstName, lastName, username, password) => {
+        const response = await api.register(firstName, lastName, username, password);
+        if (response.status === 200) {
+            authReducer({
+                type: AuthActionType.REGISTER,
+                payload: {
+                    user: response.data.user
+                }
+            });
+            history.push('/login');
+        }
+    }
+    // changing password should logout user
+    auth.changePassword = async (currentPassword, newPassword) => {
+        const response = await api.changePassword(currentPassword, newPassword);
+        if (response.status === 200) {
+            authReducer({
+                type: AuthActionType.CHANGE_PASSWORD,
+                payload: {
+                    user: response.data.user
+                }
+            });
+            history.push('/');
+        }
+    }
+    auth.deleteAccount = async (password) => {
+        const response = await api.deleteAccount(password);
+        if (response.status === 200) {
+            authReducer({
+                type: AuthActionType.DELETE_ACCOUNT,
+                payload: null
+            });
+            history.push('/');
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={auth}>
+            {props.children}
+        </AuthContext.Provider>
+    );
+}
+
+export default AuthContext;
+export { AuthContextProvider };
