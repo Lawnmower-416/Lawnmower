8803ab84979d55c2bc38265cb066307fdd51e391
SungMoY <90225118+SungMoY@users.noreply.github.com>
2022-11-04 17:30:25 -0400

    UPDATE-header-loggedin
    When a user is logged in, the header no longer shows SIGN UP/SIGN IN as options. Instead, a dropmenu is created with the user's icon.

--- client/src/components/screens/Header.js
+++ client/src/components/screens/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdClose } from "react-icons/io";
 import { useLocation } from "react-router-dom";
+import AuthContext from "../../auth";
+import { Menu } from '@headlessui/react';
+
 
 const Header = () => {
-  
+  const { auth } = useContext(AuthContext);
   const [sidebar, setSidebar] = useState(false);
   let navArray = [
     { navItem: "FAQ", to: "/faq" },
@@ -24,21 +27,57 @@ const Header = () => {
 
 
   // TODO: implement account checking, where header no longer shows Sign Up/Sign In if user is logged in
+  let topRightMenu = 
+        <div className="hidden md:flex gap-x-6 font-inter font-bold ">
+        <Link
+          to="/register"
+          className="text-lg md:text-lg lg:text-2xl 2xl:text-4xl"
+        >
+          Sign Up
+        </Link>
+        <Link
+          to="/login"
+          className="text-lg md:text-lg lg:text-2xl 2xl:text-4xl"
+        >
+          Sign In
+        </Link>
+      </div>;
+  if (auth.loggedInBool) {
+    console.log("logged in", auth.loggedIn);
+    topRightMenu = 
+    <Menu as="div" className="relative">
+      <Menu.Button>
+        AVATAR
+      </Menu.Button>
+      <Menu.Items className="absolute translate-x-0 bg-darker-gray rounded-xl shadow-lg">
+        <Menu.Item>
+          <Link to="/profile" className="block px-4 py-2 text-white text-md hover:bg-darker-gray rounded-t-xl w-full border-b-2 border-dark-gray">Profile</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to="/community" className="block px-4 py-2 text-white text-md hover:bg-darker-gray rounded-t-xl w-full border-b-2 border-dark-gray">Community</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <button onClick={auth.logout} className="block px-4 py-2 text-white text-md hover:bg-darker-gray rounded-b-xl w-full">Logout</button>
+        </Menu.Item>
+      </Menu.Items>
+    </Menu>
+  }
 
 
   return (
-    <div className="headerWrapper ">
-      <div className="flex justify-between items-center max-w-screen-2xl mx-auto py-2 header w-11/12">
+    <div className="headerWrapper">
+      <div className="flex justify-between items-center max-w-screen-2xl header">
         <Link to="/" className="flex items-center gap-1 sm:gap-3">
           <img
             src="./logo.png"
-            alt="#"
-            className="w-16 lg:w-20 2xl:w-28"
+            alt="logo"
+            className="w-16"
           />
           <h3 className="font-bold font-inter text-xl  sm:text-2xl  lg:text-4xl   2xl:text-6xl">
             Lawnmower
           </h3>
         </Link>
+
         {sidebar ? (
           <IoMdClose
             className="text-black text-2xl block md:hidden cursor-pointer"
@@ -50,6 +89,7 @@ const Header = () => {
             onClick={() => setSidebar((prev) => !prev)}
           />
         )}
+
         <div className="hidden md:flex   gap-x-6  items-center gap-12 font-inter font-bold ">
           {navArray.map((el, i) => (
             <Link
@@ -61,21 +101,13 @@ const Header = () => {
             </Link>
           ))}
         </div>
-        <div className="hidden md:flex gap-x-6 font-inter font-bold ">
-          <Link
-            to="/register"
-            className="text-lg md:text-lg lg:text-2xl 2xl:text-4xl"
-          >
-            Sign Up
-          </Link>
-          <Link
-            to="/login"
-            className="text-lg md:text-lg lg:text-2xl 2xl:text-4xl"
-          >
-            Sign In
-          </Link>
-        </div>
+
+      {
+      topRightMenu
+      }
+      
       </div>
+
       {sidebar && (
         <div className="header min-h-screen pt-5 items-center flex-col flex md:hidden pb-10">
           {navArray.map((el, i) => (
