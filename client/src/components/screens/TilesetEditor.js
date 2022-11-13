7b577775cefb5b4fbe268022708be87f75eae26b
Shreyas Prasad <shreyasprasad14@gmail.com>
2022-11-13 04:44:58 -0500

    Draw + Highlight Tile

--- client/src/components/screens/TilesetEditor.js
+++ client/src/components/screens/TilesetEditor.js
@@ -30,61 +30,6 @@ function TilesetEditor() {
 
     const [currentTile, setCurrentTile] = useState(null);
 
-    function addColor({red, blue, green}) {
-        colors.push({red, blue, green, alpha: 255});
-    }
-
-    function addTile() {
-        const newTileData = [];
-
-        for(let i = 0; i < 3; i++) {
-            const row = [];
-            for(let j = 0; j < 3; j++) {
-                row.push({red: 0, green: 0, blue: 0, alpha: 255});
-            }
-            newTileData.push(row);
-        }
-        tiles.push({_id: tiles.length.toString(), data: newTileData});
-    }
-
-    const colors = [
-        {
-            red: 255,
-            green: 0,
-            blue: 0,
-            alpha: 255,
-        },
-        {
-            red: 0,
-            green: 255,
-            blue: 0,
-            alpha: 255,
-        },
-        {
-            red: 0,
-            green: 0,
-            blue: 255,
-            alpha: 255,
-        },
-    ]
-
-    const tiles = [
-        { _id: "0", 
-            data: [
-                [[255, 0, 0], [0, 255, 0], [0, 0, 255]],
-                [[255, 0, 0], [0, 255, 0], [0, 0, 255]],
-                [[255, 0, 0], [0, 255, 0], [0, 0, 255]]
-            ] 
-        },
-        { _id: "1",
-            data: [
-                [[255, 255, 0], [0, 255, 255], [255, 255, 255]],
-                [[255, 0, 0], [0, 0, 0], [0, 0, 255]],
-                [[128, 128, 128], [55, 128, 100], [236, 23, 190]]
-            ]
-        },
-    ]
-
     //TODO: Loading Icon
     if(!tileset) {
         return <div>Loading...</div>
@@ -107,9 +52,9 @@ function TilesetEditor() {
                 setShareOpen={setShareOpen}
             />
             <div className="flex h-screen">
-                <ColorSidebar colors={colors} addColor={addColor} />
+                <ColorSidebar />
                 <TileEditor currentTile={tilesetImage.tiles[0]}/>
-                <TileSidebar tiles={[]} setCurrentTile={setCurrentTile} addTile={addTile}/>
+                <TileSidebar tiles={[]} setCurrentTile={setCurrentTile} />
             </div>
         </div>
         
--- client/src/components/screens/editor/TileEditor.js
+++ client/src/components/screens/editor/TileEditor.js
@@ -5,41 +5,61 @@ function TileEditor({currentTile}) {
     const { store } = useContext(EditorContext);
 
     const ref = useRef(null);
+
     useEffect(() => {
         if(ref && currentTile) {
-            const tileSize = store.tileset.tileSize;
-
-            const colorData = new Uint8ClampedArray(Object.values(currentTile.data));
-            //formatted as [r, g, b, a, r, g, b, a, ...]
-
-            const context = ref.current.getContext('2d');
-            //context.putImageData(new ImageData(colorData, store.tileset.tileSize, store.tileset.tileSize), 0, 0);
-
-            //draw rgba data from colorData to canvas and scale it to fit the canvas
-            const imageData = context.createImageData(tileSize, tileSize);
-            for(let i = 0; i < colorData.length; i++) {
-                imageData.data[i] = colorData[i];
-            }
-            context.putImageData(imageData, 0, 0);
-
-
+            drawTile();
         }
-    })
+    });
 
-    const zoom = (ctx, x, y) => {
-        ctx.drawImage(ref,
-            Math.min(Math.max(0, x - 5), 32 - 10),
-            Math.min(Math.max(0, y - 5), 32 - 10),
-            10, 10,
-            0, 0,
-            200, 200);
-    };
+    const drawTile = () => {
+        const tileSize = store.tileset.tileSize;
+
+        const colorData = new Uint8ClampedArray(Object.values(currentTile.data));
+        //formatted as [r, g, b, a, r, g, b, a, ...]
+
+        //context.putImageData(new ImageData(colorData, store.tileset.tileSize, store.tileset.tileSize), 0, 0);
+        const context = ref.current.getContext('2d');
+        //draw rgba data from colorData to canvas and scale it to fit the canvas
+        // const imageData = context.createImageData(tileSize, tileSize);
+        // for(let i = 0; i < colorData.length; i++) {
+        //     imageData.data[i] = colorData[i];
+        // }
+        // context.putImageData(imageData, 0, 0);
+        const data = colorData;
+        const pixelSize = 100;
+        let y = pixelSize
+        for(let y = 0; y < tileSize; y++) {
+            for(let x = 0; x < tileSize; x++) {
+                const index = (y * tileSize + x) * 4;
+                context.fillStyle = `rgba(${data[index]}, ${data[index+1]}, ${data[index+2]}, ${data[index+3]})`;
+                context.fillRect(x*pixelSize, y*pixelSize, pixelSize, pixelSize);
+            }
+        }
+    }
+
+    const highlightPixel = (e) => {
+        drawTile();
+        const context = ref.current.getContext('2d');
+        const rect = ref.current.getBoundingClientRect();
+        const x = Math.floor((e.clientX - rect.left) / 100);
+        const y = Math.floor((e.clientY - rect.top) / 100);
+        context.fillStyle = 'rgba(255, 255, 255, 0.5)';
+        context.fillRect(x*100, y*100, 100, 100);
+    }
     
     return (
         <main className="flex flex-col w-full bg-white overflow-x-hidden overflow-y-auto mb-14">
             <Toolbar />
             <div className="flex w-full mx-auto px-6 py-8 justify-center">
-                {ref && <canvas ref={ref} width="600" height="600" />}
+                {ref &&
+                    <canvas
+                        ref={ref}
+                        width="600"
+                        height="600"
+                        onMouseMove={(e) => highlightPixel(e)}
+                    />
+                }
             </div>
       </main>
     )
