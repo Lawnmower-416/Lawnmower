390345dfabb6a2ea8036f8dabbb1b9da43a3df94
Rohit Panda <rohitpanda2@yahoo.com>
2022-11-21 07:54:19 -0500

    mchanges

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,1 +1,1 @@
-export const serverUrl = 'http://localhost:5000'
+export const serverUrl = 'http://34.193.24.27/'
--- server/clientUrl.js
+++ server/clientUrl.js
@@ -1,3 +1,3 @@
-const clientUrl = 'http://localhost:3000'
+const clientUrl = 'http://34.193.24.27/'
 
 module.exports = clientUrl;
